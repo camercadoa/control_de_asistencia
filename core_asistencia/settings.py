abc7609b0030a5124a0cf9a6d4d06b897725b2b1
Camilo Mercado <mercadoarizacamilo@gmail.com>
2025-09-02 10:05:38 -0500

    Updates
    - Integración de DataTable.
    - Cambio en Colores del Aplicativo

--- core_asistencia/settings.py
+++ core_asistencia/settings.py
@@ -73,11 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core_asistencia.wsgi.application'
 
-REST_FRAMEWORK = {
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 25
-}
-
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
