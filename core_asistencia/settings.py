bf8ec7c2831db3bdb4405fd654dca4d4e0b7ea8a
Camilo Mercado <mercadoarizacamilo@gmail.com>
2025-10-08 14:50:49 -0500

    Update
    Configuración para Servidor

--- core_asistencia/settings.py
+++ core_asistencia/settings.py
@@ -129,8 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static_control/'
+STATIC_ROOT = BASE_DIR / "staticfiles_control"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
